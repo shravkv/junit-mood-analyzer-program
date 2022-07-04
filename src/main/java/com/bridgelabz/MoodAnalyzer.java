57a158dc3656e2f67cc15da64bf5f2d828bb3187
shravkv <sravan.k.vodnala@gmail.com>
2022-07-04 09:36:59 +0530

    Added:Analyze Happy or sad mood

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,5 +1,10 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
-
+    public String analyzeMood(String message) {
+        if (message.contains("Iam in Sad Mood"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
 }
